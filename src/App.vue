ef931e1d4daedb16694cf81f7ce61076113feeba
Mohammed Azzam <mohammed.mojahed93@gmail.com>
2020-09-28 02:47:12 +0300

    (2 Toolbar UI Component to create header)

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,18 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <Header/>
+    <v-app-bar app flat>
+      <Header />
     </v-app-bar>
 
-
-    <v-main>
-    </v-main>
+    <v-main> </v-main>
   </v-app>
 </template>
 
 <script>
-import Header from './components/Header';
+import Header from "./components/Header";
 
 export default {
-  name: 'App',
+  name: "App",
 
   components: {
     Header,
